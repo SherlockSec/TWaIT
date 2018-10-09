93b43f0afc454dfbc4e1d6c7706474172d1bbc95
Daniel Dixon <dan@sherlock-security.com>
2018-10-09 12:11:04 +0100

    Create twat.py

---
+++ twat.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Metadata
+__author__ = "SherlockSec";
+__version__ = "1.0";
+__license__ = "GPL-3.0";
+
+#Imports
+import sys;
+import os;
+import subprocess;
+
+
+
+if len(sys.argv) == 1:
+    print("Error: no arguments given");
+
+
+ 
